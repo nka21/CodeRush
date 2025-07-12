0f7ce7b5ae4375aa135f5c6c1a69344ee191cd6a
Kazuya <kazuya2002uk@gmail.com>
2025-07-12 10:58:02 +0900

    微調整

--- client/app/test/_components/QuizResult.tsx
+++ client/app/test/_components/QuizResult.tsx
@@ -7,12 +7,12 @@ type QuizResultProps = {
   result: QuizResult;
 };
 
-const rankIcons = ["★", "◆", "▲", "●"];
+const rankIcons = "◆";
 const rankColors = [
-  "text-yellow-400 font-bold", // 1位
-  "text-cyan-400",             // 2位
-  "text-yellow-700",           // 3位
-  "text-green-400",            // 4位以降
+  "text-yellow-400 font-bold",           // 1位
+  "text-cyan-400 font-bold",             // 2位
+  "text-yellow-700 font-bold",           // 3位
+  "text-green-400 font-bold",            // 4位以降
 ];
 
 // ダミーデータ
@@ -25,11 +25,13 @@ const players = [
 ];
 
 
+// 順位とプレイヤー名を受け取る　順位毎にソート
+
 export const QuizResultScreen = (props: QuizResultProps) => {
   const { result } = props;
   const router = useRouter();
   
-  const rank:number = 1; //暫定順位
+  const rank:number = 1; //暫定順位　　自分の順位をここに入れる
 
   return (
     <div className="text-center">
@@ -45,7 +47,7 @@ export const QuizResultScreen = (props: QuizResultProps) => {
             key={player.name}
             className={`flex items-center gap-2 py-1 ${rankColors[idx] || rankColors[3]}`}
           >
-            <span>{rankIcons[idx] || rankIcons[3]}</span>
+            <span>{rankIcons}</span>
             <span className="flex-1">{player.name}</span>
             <span className="ml-2">{result.score} pts</span>
           </div>
@@ -55,7 +57,7 @@ export const QuizResultScreen = (props: QuizResultProps) => {
 
 	  
       <div className="mt-1 flex-col flex gap-3">
-        <Button onClick={() => router.push("/")} shortcutKey={1} label=" cd ~/ && ./home" description="ホームに戻る"/>
+        <Button onClick={() => router.push("/")} shortcutKey={1} label="cd ~/ && ./home" description="ホームに戻る"/>
         <Button onClick={() => router.push("/")} shortcutKey={2} label="cat ./question.log" description="問題の確認" />
       </div>
     </div>
