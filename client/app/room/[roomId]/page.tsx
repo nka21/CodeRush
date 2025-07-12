1f3e6d297f994ee02f8b36c30621f16ce1d8b0b6
nka21 <naoki.nico@gmail.com>
2025-07-12 12:37:40 +0900

    feat: ルームページのパラメータをPromise型からオブジェクト型に変更し、非同期関数を同期関数に修正

--- client/app/room/[roomId]/page.tsx
+++ client/app/room/[roomId]/page.tsx
@@ -1,13 +1,13 @@
 import { RoomClient } from "../_components/RoomClient";
 
 type RoomPageProps = {
-  params: Promise<{
+  params: {
     roomId: string;
-  }>;
+  };
 };
 
-export default async function RoomPage(props: RoomPageProps) {
-  const { roomId } = await props.params;
+export default function RoomPage(props: RoomPageProps) {
+  const { roomId } = props.params;
 
   return <RoomClient roomId={roomId} />;
 }
